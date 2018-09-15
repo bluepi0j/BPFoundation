5b18697d4fdff0791b911d3da63bf51a4d6b8269
bluepi0j <vicwang95@gmail.com>
2018-09-15 11:17:12 -0400

    init

---
+++ Example/Pods/Target Support Files/BPFoundation/BPFoundation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BPFoundation : NSObject
+@end
+@implementation PodsDummy_BPFoundation
+@end
---
+++ Example/Pods/Target Support Files/Pods-BPFoundation_Example/Pods-BPFoundation_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BPFoundation_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BPFoundation_Example
+@end
